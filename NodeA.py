e381ce974ea644dc63a22b695dfdb6d337320aa6
Tugarin <tugarinxv@gmail.com>
2024-03-12 07:29:24 +0300

    сделал MVP модели "от-многих-ко-многим"

--- NodeA.py
+++ NodeA.py
@@ -1,22 +1,73 @@
 import zmq
-import time
+import zmq.asyncio
 
-context = zmq.Context()
+context = zmq.asyncio.Context() #контейнер для соектов
 
-# Сокет для отправки сообщений
-sender = context.socket(zmq.PUSH)
-sender.bind("tcp://*:5555")
+async def pub_function(port_pub):
+    '''подключение к сети в роли PUB паттерну PUB-SUB
+    '''
+    pub_socket = context.socket(zmq.PUB) # роль сокета
+    pub_socket.bind(f"tcp://*:{port_pub}") # бинд сокета на хост
+    
+    global other_ports
+    other_ports.add(port_pub) # добавление порта паба, в множество. чтобы остальные могли его слушать
+    
+    await pub_socket.send_string("подключён")
 
-# Сокет для приема ответов
-receiver = context.socket(zmq.PULL)
-receiver.connect("tcp://localhost:5556")
 
-# Отправляем сообщение узлу B
-print("Отправка сообщения узлу B...")
-sender.send(b"Привет от A!")
+def sub2all():
+    '''функция саба на все узлы в сети'''
+    sub_socket = context.socket(zmq.SUB)
+    sub_socket.subscribe("") # подписался(в данном случае на всё)
 
-# Ждем ответа от узла B
-message = receiver.recv()
-print(f"Получен ответ от узла B: {message.decode()}")
+    for port in other_ports: # можно сделать через filter() и лямбду. но пока мне в падлу
+        if port != port_pub:
+            sub_socket.connect(f"tcp://localhost:{port}") # подключился ко всем(кроме себя)
 
-time.sleep(1)  # Имитация задержки
+    while True:
+        message = sub_socket.recv_string() # получение сообщений в str()
+        
+        print(f"получено: {message}")
+
+
+# Порт для PUB сокета текущего узла
+port_pub = "5555"
+
+# Порты PUB сокетов других узлов
+other_ports = {"5555","5557", "5559"}
+
+sub2all()
+pub_function(port_pub)
+
+
+
+
+# import zmq
+# import time
+
+# context = zmq.Context()
+# host = "127.0.0.1"
+# port = "5001"
+# # Сокет для отправки сообщений
+# publisher = context.socket(zmq.PUB)
+# publisher.bind(f"tcp://{host}:{port}")
+# time.sleep(1)
+# publisher.send_string("say")
+
+# print("успешно")
+
+
+
+
+# while True:
+#     publisher.send_string(input("пиши: ").encode())
+#     print("sent")
+#     msg = publisher.recv()
+#     print("собеседник: ", msg.decode())
+    # # Отправляем сообщение NodeB
+    # publisher.send("Привет, NodeB!".encode())  # Используйте publisher, а не socket
+    # print("NodeA: Сообщение отправлено NodeB.")
+
+    # # Ждем сообщения от NodeB
+    # msg = publisher.recv()  # Используйте publisher, а не socket
+    # print(f"NodeA: Получено сообщение от NodeB: {msg.decode()}")
--- NodeB.py
+++ NodeB.py
@@ -1,20 +1,68 @@
 import zmq
+import zmq.asyncio
 
-context = zmq.Context()
+context = zmq.asyncio.Context() #контейнер для соектов
 
-# Сокет для приема сообщений
-receiver = context.socket(zmq.PULL)
-receiver.bind("tcp://*:5556")
+async def pub_function(port_pub):
+    '''подключение к сети в роли PUB паттерну PUB-SUB
+    '''
+    pub_socket = context.socket(zmq.PUB) # роль сокета
+    pub_socket.bind(f"tcp://*:{port_pub}") # бинд сокета на хост
+    
+    global other_ports
+    other_ports.add(port_pub) # добавление порта паба, в множество. чтобы остальные могли его слушать
+    
+    await pub_socket.send_string("подключён")
 
-# Сокет для отправки ответов
-sender = context.socket(zmq.PUSH)
-sender.connect("tcp://localhost:5555")
 
-# Ждем сообщения от узла A
-message = receiver.recv()
-print(f"Получено сообщение от узла A: {message.decode()}")
+def sub2all():
+    '''функция саба на все узлы в сети'''
+    sub_socket = context.socket(zmq.SUB)
+    sub_socket.subscribe("") # подписался(в данном случае на всё)
 
-# Отправляем ответ узлу A
-print("Отправка ответа узлу A...")
-sender.send(b"Привет от B!")
+    for port in other_ports: # можно сделать через filter() и лямбду. но пока мне в падлу
+        if port != port_pub:
+            sub_socket.connect(f"tcp://localhost:{port}") # подключился ко всем(кроме себя)
 
+    while True:
+        message = sub_socket.recv_string() # получение сообщений в str()
+        
+        print(f"получено: {message}")
+
+
+# Порт для PUB сокета текущего узла
+port_pub = "5557"
+# Порты PUB сокетов других узлов
+other_ports = {"5555","5557", "5559"}
+
+sub2all()
+pub_function(port_pub)
+
+
+
+
+# import zmq
+
+# context = zmq.Context()
+# host = "localhost"
+# port = "5001"
+# # Сокет для приема сообщений
+# receiver = context.socket(zmq.SUB)
+# receiver.connect(f"tcp://{host}:{port}")
+# receiver.subscribe("")
+# print("ПОДКЛЮЧЕНО")
+# print(receiver.recv_string())
+
+
+
+
+# while True:
+#     # Ждем сообщения от NodeA
+#     msg = receiver.recv_string()
+#     print(f"собеседник: {msg.decode()}")
+
+#     # Отправляем ответ NodeA
+#     receiver.send(input("ПИШИ: ").encode())
+#     print("sent")
+
+#     time.sleep(1)
