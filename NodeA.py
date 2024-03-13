a6b2b3d48bd6b252e6a80d6f83e26b87fc6fbf54
Tugarin <tugarinxv@gmail.com>
2024-03-13 20:57:27 +0300

    убрал async, поставил threading

--- NodeA.py
+++ NodeA.py
@@ -1,21 +1,23 @@
 import zmq
-import zmq.asyncio
+import time
+import threading
 
-context = zmq.asyncio.Context() #контейнер для соектов
+context = zmq.Context() #контейнер для сокетов
 
-async def pub_function(port_pub):
+def pub_function(port_pub):
     '''подключение к сети в роли PUB паттерну PUB-SUB
     '''
     pub_socket = context.socket(zmq.PUB) # роль сокета
     pub_socket.bind(f"tcp://*:{port_pub}") # бинд сокета на хост
+    time.sleep(2)
     
     global other_ports
     other_ports.add(port_pub) # добавление порта паба, в множество. чтобы остальные могли его слушать
     
-    await pub_socket.send_string("подключён")
+    pub_socket.send_string("подключён")
 
 
-def sub2all():
+def sub2all(port_pub):
     '''функция саба на все узлы в сети'''
     sub_socket = context.socket(zmq.SUB)
     sub_socket.subscribe("") # подписался(в данном случае на всё)
@@ -36,5 +38,7 @@ port_pub = "5555"
 # Порты PUB сокетов других узлов
 other_ports = {"5555","5557", "5559"}
 
-sub2all()
-pub_function(port_pub)
+sub_thread = threading.Thread(target=sub2all, args=(port_pub,))
+sub_thread.start()
+time.sleep(1)
+print(pub_function(port_pub))
