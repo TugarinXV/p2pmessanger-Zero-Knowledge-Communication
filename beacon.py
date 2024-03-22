4ebc65e6f8f5fb096c0c6ebde1f7467989105152
Tugarin <tugarinxv@gmail.com>
2024-03-22 20:24:08 +0300

    - создал класс для маяка - сделал конструктор для ясности объектов в коде - допилил сокет REP для системных сообщений клиенту - добавил сокеты PUB и SUB, для проверки онлайна - исправил косяк в создании потока для heartbeat - избавил от массива с портами - и остальное по мелочи

--- beacon.py
+++ beacon.py
@@ -1,38 +1,57 @@
 import zmq
+import time
+import threading
+import re
 
-port = "8001"
-context = zmq.Context()
-beacon = context.socket(zmq.REP)
-beacon.bind(f"tcp://*:{port}")
-available_ports = ["5556", "5557", "5558", "5559", "5560"]
-connected_users = []
+class Beacon():
+    def __init__(self, connected_users: list):
+        self.context = zmq.Context()
+        
+        self.beacon = self.context.socket(zmq.REP)
+        self.beacon.bind(f"tcp://*:8001")
+        self.heart = self.context.socket(zmq.PUB)
+        self.heart.bind("tcp://*:8002")
+        self.socket_receive = self.context.socket(zmq.SUB)
+        self.socket_receive.subscribe("")
 
-# Функция для чтения события монитора
-def recv_monitor_message(monitor, flags=0, recv_timeout=1000):
-    if monitor.poll(recv_timeout):
-        msg = monitor.recv(flags)
-        event = zmq.utils.monitor.parse_monitor_message(msg)
-        return event
-    else:
-        raise zmq.Again()
-    
-while True:
-    # Ожидание подключения нового пользователя
-    message = beacon.recv_string(zmq.NOBLOCK)
-    try:
-        if message:
-            user_id, user_port = message.split('|')
-            if user_id not in connected_users:
-                connected_users.append(user_id)  # Добавление пользователя в список подключённых
-                print(f"Новый пользователь {user_id} подключён на порту {user_port}")
+        def heartbeat():
+            while True:
+                self.heart.send_string("ilb&lbp(*p)b8Y78BR6_+bpb*yb(*by(b9\n")
+                time.sleep(4)
+                message = self.beacon.recv_string()
+                if "Daddy! i`m fine!" in message:
+                    regex = r"Daddy! i`m fine!(\d+)"
+                    match = re.search(regex, message)
+                    self.сonnected_users.append(match)
+
+        heartbeat_thread = threading.Thread(target=heartbeat)
+        heartbeat_thread.daemon = True
+        heartbeat_thread.start()
+
+        # Функция для чтения события монитора
+        def recv_monitor_message(monitor, flags=0, recv_timeout=1000):
+            if monitor.poll(recv_timeout):
+                msg = monitor.recv(flags)
+                event = zmq.utils.monitor.parse_monitor_message(msg)
+                return event
+            else:
+                raise zmq.Again()
             
-            # Отправка списка портов, исключая порт текущего пользователя
-            ports_to_send = [port for port in available_ports if port != user_port]
-            beacon.send_json(ports_to_send)
-    except zmq.Again:
-        pass  # Нет входящих сообщений
-        # Здесь можно добавить логику проверки онлайн-статуса портов
-        # ...
+        while True:
+            # Ожидание подключения нового пользователя
+            message = self.beacon.recv_string(zmq.NOBLOCK)
+            try:
+                if message:
+                    user_id, user_port = message.split('|')
+                    if user_id not in connected_users:
+                        connected_users.append(user_id)  # Добавление пользователя в список подключённых
+                        print(f"Новый пользователь {user_id} подключён на порту {user_port}")
+                    
+                    # Отправка списка портов, исключая порт текущего пользователя
+                    ports_to_send = [port for port in connected_users if port != user_port]
+                    self.beacon.send_json(ports_to_send)
+            except zmq.Again:
+                pass  # Нет входящих сообщений
+                # Здесь можно добавить логику проверки онлайн-статуса портов
 
-# Запуск маяка на порту 9999
-beacon(9999, available_ports)
+
